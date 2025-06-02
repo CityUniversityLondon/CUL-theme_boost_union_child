a0e965ffc70956968290d9018ce5d40ec43bf02a
David Scotson <david.scotson@synergy-learning.com>
2025-06-02 16:55:55 +0100

    add academic year task to theme

---
+++ db/tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+$tasks = [
+    [
+        'classname' => 'theme_boost_union_child\task\add_academicyear_custom_field',
+        'blocking' => '*',
+        'minute' => '0',
+        'hour' => '0',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    ]
+];
